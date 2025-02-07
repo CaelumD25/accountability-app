785eb2bcad77be175194c4fcd20fcaeae01a7f46
CaelumD25 <caelumdudek25@gmail.com>
2025-02-06 21:12:41 -0800

    fix: Fixed configs

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/" element={<Items/>} />
+        <Route path="/Items" element={<Items/>} />
       </Routes>
     </Router>
   )
