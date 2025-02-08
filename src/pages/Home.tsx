4fb56be672d98a81fb047c4b31297fd2d08db9ce
CaelumD25 <caelumdudek25@gmail.com>
2025-02-07 20:19:55 -0800

    More debugging features

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,15 @@ import {useNavigate} from "react-router-dom";
 
 
 const Home = () => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState<[]|string>("Loading...")
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetch("/api/Items")
+  const callItems = () => {
+    fetch("/api/items")
       .then((response) => {
         if (!response.ok) {
+          setItems(response.json());
           throw new Error("Network response was not ok");
         }
         return response.json();
@@ -21,8 +22,19 @@ const Home = () => {
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
+  }
+
+  useEffect(() => {
+    callItems();
   }, []);
 
-  return <>Items: {items} <button onClick={() => navigate("/Items")}>Add</button></>
+  return <>Items: {items}
+    <div>
+      <button onClick={() => navigate("/Items")}>Go to Items</button>
+    </div>
+    <div>
+      <button onClick={() => callItems()}>Request API Again</button>
+    </div>
+  </>
 }
 export default Home;
